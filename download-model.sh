512372dfe1ebc69554303b44f01a4606cbba91f3
sunayanapamula-gif <sunayanapamula@gmail.com>
2026-03-13 15:25:16 +0530

    Update download-model.sh

--- download-model.sh
+++ download-model.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
-mkdir -p models
-curl -L -o models/Llama-3.2-3B-Instruct-Q4_K_M.gguf \
-  https://huggingface.co/TheBloke/Llama-3.2-3B-Instruct-GGUF/resolve/main/Llama-3.2-3B-Instruct-Q4_K_M.gguf
+git submodule update --init --recursive
+cd llama.cpp
+make -j
+cd ..
+bash download-model.sh
